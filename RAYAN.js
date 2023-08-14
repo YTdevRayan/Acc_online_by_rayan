4287a8086c2fe409252f66989704ccb417b2e1f0
Glitch (thin-maze-desk) <none>
2023-08-14 21:48:21 +0000

    🏪🌱 Checkpoint ./RAYAN.js:60674445/2200

--- RAYAN.js
+++ RAYAN.js
@@ -1,23 +1,29 @@
 //===============RAYAN============//
 const Discord = require("discord.js");
 const RAYAN = new Discord.Client();
+const RAYAN2 = new Discord.Client();
+const RAYAN3 = new Discord.Client();
 //===============RAYAN============//
 RAYAN.on("ready", () => {
   console.log(`Online In Servers`);
-  let statuses = [`STREAMING BY DEV RAYAN`];
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
   setInterval(function () {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     RAYAN.user.setActivity(STREAMING, {
-      type: "STREAMING",
+      type: "PLAYING",
       url: "https://www.twitch.tv/faith",
     });
   }, 2000);
 });
 //===============RAYAN============//
 RAYAN.on("ready", () => {
-  var join = RAYAN.channels.get(""); /// id voice
+  var join = RAYAN.channels.get("1049044395573387325"); /// id voice
   if (join) join.join();
 });
 //===============RAYAN============//
-RAYAN.login(""); /////TOKEN DANE
+RAYAN.login(
+  "MTE0MDc2MTk2NDY3NTYxMjc4Mg.Gblpay.u-7_3Y-nbumkPj7D684mnzqF9ko6fTYSiXEyBc"
+);
+RAYAN2.login("");
+RAYAN3.login(""); /////TOKEN DANE
 //===============RAYAN============//
