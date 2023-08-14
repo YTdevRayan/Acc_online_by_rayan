fdf4e7f822bbffed0543facacde433e05e10ad95
Glitch (thin-maze-desk) <none>
2023-08-14 23:16:55 +0000

    🏣🐥 Checkpoint ./RAYAN.js:60674445/7249

--- RAYAN.js
+++ RAYAN.js
@@ -28,6 +28,39 @@ RAYAN2.on("ready", () => {
     });
   }, 2000);
 });
+RAYAN3.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN3.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
+RAYAN4.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN4.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
+RAYAN5.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN5.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
 //===============RAYAN============//
 RAYAN.on("ready", () => {
   var join = RAYAN.channels.get("1049044395573387325"); /// id voice
@@ -42,7 +75,7 @@ RAYAN3.on("ready", () => {
   if (join) join.join();
 });
 RAYAN4.on("ready", () => {
-  var join = RAYAN.channels.get("1049044395573387325"); /// id voice
+  var join = RAYAN.channels.get("1049044395573387325");
   if (join) join.join();
 });
 RAYAN4.on("ready", () => {
@@ -54,8 +87,12 @@ RAYAN5.on("ready", () => {
   if (join) join.join();
 });
 //===============RAYAN============//
-RAYAN.login("MTE0MDc2MTk2NDY3NTYxMjc4Mg.Gblpay.u-7_3Y-nbumkPj7D684mnzqF9ko6fTYSiXEyBc");
-RAYAN2.login("MTE0MDM2OTY5NzMwOTc0MTE5OA.GPYlOu.kPYXfDi5U0W8PvC-movqiNuIgplXJJoFVau16g");
+RAYAN.login(
+  "MTE0MDc2MTk2NDY3NTYxMjc4Mg.Gblpay.u-7_3Y-nbumkPj7D684mnzqF9ko6fTYSiXEyBc"
+);
+RAYAN2.login(
+  "MTE0MDM2OTY5NzMwOTc0MTE5OA.GPYlOu.kPYXfDi5U0W8PvC-movqiNuIgplXJJoFVau16g"
+);
 RAYAN3.login("");
 RAYAN4.login("");
 RAYAN5.login("");
