f0c63523d0ee7db95228e0b91d5a080b4ed7d96d
Glitch (thin-maze-desk) <none>
2023-08-14 23:55:33 +0000

    🛀❄️ Checkpoint ./RAYAN.js:60674445/3213

--- RAYAN.js
+++ RAYAN.js
@@ -5,6 +5,11 @@ const RAYAN2 = new Discord.Client();
 const RAYAN3 = new Discord.Client();
 const RAYAN4 = new Discord.Client();
 const RAYAN5 = new Discord.Client();
+const RAYAN6 = new Discord.Client();
+const RAYAN7 = new Discord.Client();
+const RAYAN8 = new Discord.Client();
+const RAYAN9 = new Discord.Client();
+const RAYAN10 = new Discord.Client();
 //===============RAYAN============//
 RAYAN.on("ready", () => {
   console.log(`Online In Servers`);
@@ -61,6 +66,63 @@ RAYAN5.on("ready", () => {
     });
   }, 2000);
 });
+RAYAN6.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN6.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+
+  }, 2000);
+
+});
+RAYAN7.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN7.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
+RAYAN8.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN8.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
+RAYAN9.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN9.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
+RAYAN10.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`By DEV RAYAN & KURDISH PIRATE`];
+  setInterval(function () {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    RAYAN10.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith",
+    });
+  }, 2000);
+});
 //===============RAYAN============//
 RAYAN.on("ready", () => {
   var join = RAYAN.channels.get("1049044395573387325"); /// id voice
@@ -75,21 +137,42 @@ RAYAN3.on("ready", () => {
   if (join) join.join();
 });
 RAYAN4.on("ready", () => {
-  var join = RAYAN.channels.get("1049044395573387325");
-  if (join) join.join();
-});
-RAYAN4.on("ready", () => {
-  var join = RAYAN.channels.get("1049044395573387325");
+  var join = RAYAN2.channels.get("1049044395573387325");
   if (join) join.join();
 });
 RAYAN5.on("ready", () => {
-  var join = RAYAN.channels.get("1049044395573387325");
+  var join = RAYAN5.channels.get("1049044395573387325");
   if (join) join.join();
 });
-//===============RAYAN============//
+RAYAN6.on("ready", () => {
+  var join = RAYAN6.channels.get("1049044395573387325");
+  if (join) join.join();
+});
+RAYAN7.on("ready", () => {
+  var join = RAYAN7.channels.get("1049044395573387325");
+  if (join) join.join();
+});
+RAYAN8.on("ready", () => {
+  var join = RAYAN8.channels.get("1049044395573387325");
+  if (join) join.join();
+});
+RAYAN9.on("ready", () => {
+  var join = RAYAN9.channels.get("1049044395573387325");
+  if (join) join.join();
+});
+RAYAN10.on("ready", () => {
+  var join = RAYAN10.channels.get("1049044395573387325");
+  if (join) join.join();
+});
+//==============RAYAN============//
 RAYAN.login("MTE0MDc2MTk2NDY3NTYxMjc4Mg.Gblpay.u-7_3Y-nbumkPj7D684mnzqF9ko6fTYSiXEyBc");
 RAYAN2.login("MTE0MDM2OTY5NzMwOTc0MTE5OA.GPYlOu.kPYXfDi5U0W8PvC-movqiNuIgplXJJoFVau16g");
 RAYAN3.login("MTEzNjY4NDIwMzUwNTg4MTE2OA.GBA1gL.giDl6GO4eePCazreJWUoY8HR-1sYVmAjcEDeaU");
 RAYAN4.login("MTEzNjY5NDIxMTI2NjIyNDIwOA.GYbWJG.jwLLbjU9bC97TwHBSCNn3AU6_gkkhsdUvStmBA");
 RAYAN5.login("MTEzNjY5MTA4MDg0NzQzMzc3OA.G4v6PS.dq1ANQEwV_cu-aA54zJaDPMWbyRiV7p_SJFL_s");
+RAYAN6.login("MTEzNjY4ODY4Mjk1NTQ2MDcwMA.Ggesk0.Hesh_VZG4SCozWrus7mRhtdGoriy-3ycUi1qvc");
+RAYAN7.login("MTEzNjY4NzA3ODIyMzEzMDcxNA.GGLY2E.3e-M8MpnCA_iv8DKiWQGJSXBFDMoWx1q_GK6N4");
+RAYAN8.login("");
+RAYAN9.login("");
+RAYAN10.login("");
 //===============RAYAN============//
